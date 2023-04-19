ac9d72f98aa70e4782f22756adfe1888fa76754b
Roberto Duran <shaggyrd+github@gmail.com>
2023-04-19 17:47:52 -0500

    refactoring remove duplicate interfaces

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,7 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
 import apiClient, { FetchResponse } from "../services/api-client";
-interface Platform {
+
+export interface Platform {
   id: number;
   name: string;
   slug: string;
