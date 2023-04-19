ae4f6bccbdc4ee75b232a174611179d31dc9e2ee
Roberto Duran <shaggyrd+github@gmail.com>
2023-04-19 16:53:27 -0500

    upgrade libs and add reactquery dev tools

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import App from './App'
 import theme from './theme'
 import './index.css'
@@ -14,6 +15,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <ChakraProvider theme={theme}>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <App />
+        <ReactQueryDevtools />
       </ChakraProvider>
     </QueryClientProvider>
   </React.StrictMode>
